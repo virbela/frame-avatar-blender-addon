759a84c66cd2c099c3e33104e7e41932dda8cff3
Eric Eisaman <eric.eisaman@live.com>
2021-11-25 11:42:20 -0500

    added control flow details to create bake targets module

--- sources/modules/create_bake_targets.py
+++ sources/modules/create_bake_targets.py
@@ -21,8 +21,17 @@ def get_rgb(morphs : list) -> list:
     # use GetByNameSubstring("__TakesColor", morphs)
     return bpy.data.objects
 
-def create_variant(object):
-    pass
+def create_variant(object,i) :
+    variant = None
+    # variant = object.copy()
+    # variant.name = object.name + "__Variant_" + str(i)
+    return variant
+
+def create_n_variants(object, n) -> list:
+    variants = []
+    for i in range(1, n):
+        variants.append(create_variant(object,i))
+    return variants
 
 def get_variants(layer : list) -> list:
     print("Get Variants")
@@ -32,9 +41,28 @@ def get_variants(layer : list) -> list:
         if "__Variants_" in layer[i].name:
             n = int_after_substring(layer[i].name, "__Variants_")
             print("Variants: " + str(n))
+            variants.extend(create_n_variants(layer[i], n-1))
         continue
     return variants
     print("End Get Variants")
+
+def n_equal_sum_partitions(n :int, lst: list, d: float) -> list:
+    partitions = []
+    # returns list of lists containing the partitions of the grayscale_group
+    return partitions
+
+def partition_grayscale_group(grayscale_group : list) -> None:
+    r_layer = []
+    g_layer = []
+    b_layer = []
+    # Partition grayscale_group into r_layer, g_layer, b_layer
+    partitions = n_equal_sum_partitions(3, grayscale_group, 1.0)
+    r_layer = partitions[0]
+    g_layer = partitions[1]
+    b_layer = partitions[2]
+    state.set_r_layer(r_layer)
+    state.set_g_layer(g_layer)
+    state.set_b_layer(b_layer)
 
 def create_bake_targets()-> None:
     print("Create Bake Targets")
