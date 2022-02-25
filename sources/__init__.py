84828bf34eaeb7c52ce90664558ed6a24a82156b
ranjian0 <karanjaichungwa@gmail.com>
2022-02-25 03:45:24 +0300

    cleanup indentations

--- sources/__init__.py
+++ sources/__init__.py
@@ -1,13 +1,13 @@
 bl_info = {
-	"name": "Homeomorphic avatar creator",
-	"description": "Homeomorphic avatar creation tools",
-	"author": "Martin Petersson",
-	"version": (0, 0, 1),
-	"blender": (2, 92, 2),
-	"location": "View3D",
-	"warning": "",
-	"wiki_url": "",
-	"category": "Avatars"
+    "name": "Homeomorphic avatar creator",
+    "description": "Homeomorphic avatar creation tools",
+    "author": "Martin Petersson",
+    "version": (0, 0, 1),
+    "blender": (2, 92, 2),
+    "location": "View3D",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Avatars"
 }
 #dependencies: UVPackmaster 2.5.8
 
@@ -25,26 +25,26 @@ from .properties import HomeomorphicProperties, UIStateProperty
 
 # Addon registration
 def register():
-	for cls in pending_classes:
-		bpy.utils.register_class(cls)
+    for cls in pending_classes:
+        bpy.utils.register_class(cls)
 
-	bpy.types.Scene.homeomorphictools = bpy.props.PointerProperty(type=HomeomorphicProperties)
-	bpy.types.Scene.ui_state = bpy.props.PointerProperty(type=UIStateProperty)
-	#ISSUE-5: We should have a save handler that makes sure that images and other large resources are saved the way we want.
-	#	The reasoning here is that if multiple artists need to send a blender file back and forth and the textures are unlikely to change we could rely on a version managing tool for synchronization.
-	#	Currently the artists are not using such a tool and this feature will therefore be labelled as future-feature.
-	#	Note: `bpy.app.handlers.save_pre.append(...)`  and `load_post` for after loading
-	#	labels: future-feature
+    bpy.types.Scene.homeomorphictools = bpy.props.PointerProperty(type=HomeomorphicProperties)
+    bpy.types.Scene.ui_state = bpy.props.PointerProperty(type=UIStateProperty)
+    #ISSUE-5: We should have a save handler that makes sure that images and other large resources are saved the way we want.
+    #	The reasoning here is that if multiple artists need to send a blender file back and forth and the textures are unlikely to change we could rely on a version managing tool for synchronization.
+    #	Currently the artists are not using such a tool and this feature will therefore be labelled as future-feature.
+    #	Note: `bpy.app.handlers.save_pre.append(...)`  and `load_post` for after loading
+    #	labels: future-feature
 
 
 def unregister():
-	del bpy.types.Scene.ui_state
-	del bpy.types.Scene.homeomorphictools
-	for cls in pending_classes:
-		bpy.utils.unregister_class(cls)
+    del bpy.types.Scene.ui_state
+    del bpy.types.Scene.homeomorphictools
+    for cls in pending_classes:
+        bpy.utils.unregister_class(cls)
 
 
 if __name__ == '__main__':
-	# XXX ScriptWatcher hook for local development
+    # XXX ScriptWatcher hook for local development
     import os; os.system("clear")
     register()	
