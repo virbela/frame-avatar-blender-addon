61f6d3e1f09c3a1a90d90be82f0f4f8b1b8311e4
ranjian0 <karanjaichungwa@gmail.com>
2022-03-17 09:55:23 +0300

    implement final atlas composite

--- sources/operators/ops_export.py
+++ sources/operators/ops_export.py
@@ -2,10 +2,15 @@ import os
 import bpy
 import json
 from ..logging import log_writer as log
-from ..helpers import require_bake_scene
+from ..helpers import require_bake_scene, require_work_scene
 from ..mesh_utilities import uv_transformation_calculator, get_uv_map_from_mesh
 
+
 def export(operator, context, ht):
+    export_glb(context)
+    composite_atlas(context)
+
+def export_glb(context):
     obj = context.active_object
     obj.hide_set(False)
     obj.hide_viewport = False 
@@ -109,3 +114,58 @@ def export(operator, context, ht):
     )
 
     bpy.ops.object.delete()
+
+
+def composite_atlas(context):
+    work_scene = require_work_scene(context)
+    # switch on nodes and get reference
+
+    work_scene.use_nodes = True
+    tree = work_scene.node_tree
+
+    # clear default nodes
+    for node in tree.nodes:
+        tree.nodes.remove(node)
+
+    # create input image node
+    # TODO(ranjian0) Ensure images exists, warn otherwise
+    image_node_r = tree.nodes.new(type='CompositorNodeImage')
+    image_node_r.image = bpy.data.images['atlas_intermediate_red']
+    image_node_r.location = 0,0
+
+    image_node_g = tree.nodes.new(type='CompositorNodeImage')
+    image_node_g.image = bpy.data.images['atlas_intermediate_green']
+    image_node_g.location = 150,-150
+
+    image_node_b = tree.nodes.new(type='CompositorNodeImage')
+    image_node_b.image = bpy.data.images['atlas_intermediate_blue']
+    image_node_b.location = 300,-300
+
+    # create combine rgba node
+    comb_node = tree.nodes.new('CompositorNodeCombRGBA')   
+    comb_node.location = 500,0
+
+    # Create file output node
+    file_node = tree.nodes.new('CompositorNodeOutputFile')   
+    file_node.location = 700,0
+    file_node.format.file_format = 'JPEG'
+    file_node.format.quality = 100
+    file_node.base_path = os.path.dirname(bpy.data.filepath)
+    file_node.file_slots[0].path = "useAtlas"
+
+    # link nodes
+    links = tree.links
+    links.new(image_node_r.outputs[0], comb_node.inputs[0])
+    links.new(image_node_g.outputs[0], comb_node.inputs[1])
+    links.new(image_node_b.outputs[0], comb_node.inputs[2])
+    links.new(comb_node.outputs[0], file_node.inputs[0])
+
+    bpy.ops.render.render(use_viewport=True)
+
+    dirname = os.path.dirname(bpy.data.filepath)
+    for f in os.listdir(dirname):
+        if 'useAtlas' in f:
+            os.rename(
+                os.path.join(dirname, f),
+                os.path.join(dirname, 'useAtlas.jpg')
+            )
