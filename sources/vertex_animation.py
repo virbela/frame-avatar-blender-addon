45be2fa2f10abc9e40f2294792cf56d5b8a63c4a
ranjian0 <karanjaichungwa@gmail.com>
2022-06-01 11:05:51 +0300

    fix depsgraph scene when evaluating animation frames

--- sources/vertex_animation.py
+++ sources/vertex_animation.py
@@ -1,8 +1,9 @@
 import bpy 
-import uuid
 import bmesh
-import numpy
 from bpy.types import Action, Context, Object
+
+from .helpers import require_bake_scene
+
 
 def generate_vat_from_object(context: Context, object: Object):
     result = []
@@ -10,49 +11,39 @@ def generate_vat_from_object(context: Context, object: Object):
     vertex_count = len(object.data.vertices)
     for action in actions:
         meshes = get_per_frame_mesh(context, action, object)
-
-        export_mesh_data = meshes[0].copy()
-        create_export_mesh_object(context, export_mesh_data)
-
         offsets = get_vertex_data(meshes)
-        texture_size = vertex_count, action.frame_range.y - action.frame_range.x
-        tex = bake_vertex_data(action.name, offsets, texture_size)
+        texture_size = vertex_count, int(action.frame_range.y - action.frame_range.x)
+        tex = bake_vertex_data(f"{object.name}_{action.name}", offsets, texture_size)
         result.append(tex)
+    # reset frame
+    context.scene.frame_set(1)
     return result
 
 
 def get_per_frame_mesh(context: Context, action: Action, object: Object):
     meshes = []
-    for i in range(*tuple(map(int, action.frame_range))):
-        context.scene.frame_set(i)
-        depsgraph = context.evaluated_depsgraph_get()
-        bm = bmesh.new()        
+    sx, sy = action.frame_range
+    object.animation_data.action = action
+    bakescene = require_bake_scene(context)
+
+    # range stop is exclusive, so add one
+    for i in range(int(sx), int(sy + 1)):
+        bakescene.frame_set(i)
+        depsgraph = bakescene.view_layers[0].depsgraph
+
+        bm = bmesh.new()
         eval_object = object.evaluated_get(depsgraph)
         me = bpy.data.meshes.new_from_object(eval_object)
         me.transform(object.matrix_world)
         bm.from_mesh(me)
         bpy.data.meshes.remove(me)
-        me = bpy.data.meshes.new("mesh")
+        
+        me = bpy.data.meshes.new(f"{object.name}_{action.name}_animmesh{i}")
         bm.to_mesh(me)
         bm.free()
         me.calc_normals()
         meshes.append(me)
     return meshes
-
-
-def create_export_mesh_object(context, me):
-    """Return a mesh object with correct UVs"""
-    while len(me.uv_layers) < 2:
-        me.uv_layers.new()
-    uv_layer = me.uv_layers[1]
-    uv_layer.name = f"vertex_anim_{uuid.uuid4()}"
-    for loop in me.loops:
-        uv_layer.data[loop.index].uv = (
-            (loop.vertex_index + 0.5)/len(me.vertices), 128/255
-        )
-    ob = bpy.data.objects.new(f"export_mesh_{uuid.uuid4()}", me)
-    context.scene.collection.objects.link(ob)
-    return ob
 
 
 def get_vertex_data(meshes):
@@ -63,7 +54,7 @@ def get_vertex_data(meshes):
         for v in me.vertices:
             offset = v.co - original[v.index].co
             x, y, z = offset
-            offsets.extend((x, y, z))
+            offsets.extend((x, y, z, 1))
         if not me.users:
             bpy.data.meshes.remove(me)
     return offsets
@@ -78,7 +69,6 @@ def bake_vertex_data(name, offsets, size):
         height=height,
         float_buffer=True
     )
-    
     offset_texture.pixels = offsets
     return offset_texture
 
