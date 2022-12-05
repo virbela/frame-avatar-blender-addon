898c3f94332e9e5fe62eaf6fe4f9f288fb1a6a1d
ranjian0 <karanjaichungwa@gmail.com>
2022-12-05 17:23:30 +0300

    update weights and bone export

--- sources/weightexport.py
+++ sources/weightexport.py
@@ -2,7 +2,6 @@ import os
 import bpy
 import json 
 import numpy
-from mathutils import Matrix
 from .properties import HomeomorphicProperties
 from .helpers import get_animation_objects, require_bake_scene, get_gltf_export_indices
 
@@ -25,59 +24,34 @@ class WeightExporter:
 
         self.set_weights()
         self.set_transforms()
-        # self.save_weights_to_png()
-        # self.save_transforms_to_png()
-        # self.export_weights_json()
-        # self.export_transforms_json()
 
     def set_weights(self):
+        export_indices = get_gltf_export_indices(self.ht.avatar_object)
+
         for obj in self.animated_objects:
             self.weights[obj.name] = dict()
-            for bone_name in self.bones:
-                if bone_name not in obj.vertex_groups:
-                    self.weights[obj.name][bone_name] = list()
-                    continue 
 
-                self.weights[obj.name][bone_name] = list()
-                wk = self.weights[obj.name][bone_name]
-                group = obj.vertex_groups[bone_name]
+            tmp_map = dict()
+            for v in obj.data.vertices:
+                groups = sorted(v.groups, key=lambda x: -x.weight)[:4]
+                total_weight = sum((x.weight for x in groups), 0.0) or 1.0
+                bones = ([x.group for x in groups] + [0, 0, 0, 0])[:4]
+                weights = ([x.weight / total_weight for x in groups] + [0.0, 0.0, 0.0, 0.0])[:4]
+                
+                all_weights = [0.0] * len(self.bones)
+                for idx, bname in enumerate(self.bones):
+                    if bname not in obj.vertex_groups:
+                        continue
+                    
+                    vg = obj.vertex_groups[bname]
+                    if vg.index in bones:
+                        all_weights[idx] = weights[bones.index(vg.index)]
 
-                for i, v in enumerate(obj.data.vertices):
-                    for g in v.groups:
-                        if g.group == group.index:
-                            wk.append((i, g.weight))
-        self.post_process_weights()
+                tmp_map[v.index] = all_weights
 
-    def post_process_weights(self):
-        num_verts = len(self.animated_objects[0].data.vertices)
-
-        # set missing weights to zero
-        def fill_weights(weights):
-            result = []
-            wd = {w[0]:w[1] for w in weights}
-            for i in range(num_verts):
-                if wd.get(i):
-                    result.append((i, wd[i]))
-                else:
-                    result.append((i, 0.0))
-            weights[:] = result
-
-        # blender indices to gltf indices
-        export_indices = get_gltf_export_indices(self.ht.avatar_object)
-        def weights_with_indices(weights):
-            if not weights:
-                weights[:] = [0.0] * len(export_indices)
-                return
-
-            nw = numpy.array([k[1] for k in weights], dtype=float)
-            nww = nw[export_indices]
-            weights[:] = list(nww)
-        
-        for _, group in self.weights.items():
-            for _, weights in group.items():
-                if weights and len(weights) < num_verts:
-                    fill_weights(weights)
-                weights_with_indices(weights)
+            # convert verts to export indices
+            for i, index in enumerate(export_indices):
+                self.weights[obj.name][str(i)] = tmp_map[index]
 
     def export_weights_json(self):
         filepath = bpy.data.filepath
@@ -86,32 +60,30 @@ class WeightExporter:
             json.dump(self.weights, file, indent=4)
 
     def set_transforms(self):
-        def get_bone_mat(arm, pose_bone):
-            # blender z-up to babylon y-up
-            axis_basis_change = Matrix(
-                ((1.0, 0.0, 0.0, 0.0), (0.0, 0.0, 1.0, 0.0), (0.0, -1.0, 0.0, 0.0), (0.0, 0.0, 0.0, 1.0))
-            )
+        bakescene = require_bake_scene(self.context)
 
-            # get inverse bind matrix
-            # mat = (axis_basis_change @ arm.matrix_world @ pose_bone.bone.matrix_local).inverted_safe()
-            mat = (axis_basis_change @ pose_bone.matrix).inverted_safe()
-            return [mat[i][j] for i in range(4) for j in range(4)]
+        def get_bone_mat(pose_bone):
+            mat = pose_bone.matrix @ pose_bone.bone.matrix_local.inverted()
+
+            # XXX Export Matrix in column major format
+            return [round(mat[j][i], 4) for i in range(4) for j in range(4)]
 
         for action in bpy.data.actions:
+            # if action.name == 'tpose':
+            #     continue
+
+            self.armature.animation_data.action = action
             self.transforms[action.name] = list()
             sx, sy = action.frame_range
-            bakescene = require_bake_scene(self.context)
             if (sy - sx) > 1:
                 # range stop is exclusive, so add one if animation has more than one frame
                 sy += 1
 
             for i in range(int(sx), int(sy)):
                 bakescene.frame_set(i)
-                depsgraph = bakescene.view_layers[0].depsgraph
-                eval_arm = self.armature.evaluated_get(depsgraph)
-
-                quats = [get_bone_mat(eval_arm, eval_arm.pose.bones[bname]) for bname in self.bones]
-                self.transforms[action.name].append(sum(quats, []))
+                self.context.view_layer.update()
+                mats = [get_bone_mat(self.armature.pose.bones[bname]) for bname in self.bones]
+                self.transforms[action.name].append(sum(mats, []))
 
     def export_transforms_json(self):
         filepath = bpy.data.filepath
