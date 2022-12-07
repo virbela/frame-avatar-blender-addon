12c7042d736f89e8f768aadc2e0885a462e1747c
ranjian0 <karanjaichungwa@gmail.com>
2022-12-07 13:22:55 +0300

    new test setup

---
+++ tests/__main__.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import unittest
+
+tests_dir = os.path.dirname(__file__)
+addon_dir = os.path.dirname(tests_dir)
+
+sys.path.insert(0, tests_dir)
+sys.path.insert(0, addon_dir)
+
+import tools
+try:
+    import test_helpers
+except Exception:
+    # XXX Error importing test modules.
+    # Print Traceback and close blender process
+    import traceback
+
+    traceback.print_exc()
+    sys.exit()
+
+
+def main():
+    # Load the addon module
+    tools.LoadModule(os.path.join(addon_dir, "__init__.py"))
+    print('-' * 70, end="\n\n")
+
+    # initialize the test suite
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    # add tests to the test suite
+    suite.addTests(loader.loadTestsFromModule(test_helpers))
+
+    # initialize a runner, pass it your suite and run it
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
+
+    # close blender process
+    sys.exit()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_helpers.py
@@ -0,0 +1,6 @@
+import src 
+import unittest
+
+class TestUtilsCommon(unittest.TestCase):
+    def test_equal(self):
+        self.assertTrue(True)
