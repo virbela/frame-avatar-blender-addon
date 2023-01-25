90f5789bd2df0857a217c8270e701df979e91142
ranjian0 <karanjaichungwa@gmail.com>
2023-01-25 11:42:50 +0300

    remove todo

--- src/ui/elements.py
+++ src/ui/elements.py
@@ -2,12 +2,6 @@ import bpy
 from ..utils.properties import UV_ISLAND_MODES
 from ..utils.helpers import get_homeomorphic_tool_state
 
-#ISSUE: Some collections are referenced in a problematic way
-#	Currently we use indices to refer to items in collections but since items can move around (check if this is true) our indices can be wrong.
-#	When an index becomes the wrong thing without any error occuring this can confuse the user so we should either use a system with UUID or see if there are another better way of dealing with this problem.
-#	labels: needs-research
-
-#TODO - use a similar pattern for the draw call as we use for operators (see contribution note 3)
 
 class FRAME_UL_bake_variants(bpy.types.UIList):
 	def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
@@ -33,6 +27,7 @@ class FRAME_UL_bake_targets(bpy.types.UIList):
 class FRAME_UL_bake_groups(bpy.types.UIList):
 	def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
 		layout.prop(item, 'name', icon='UNLINKED', text='', emboss=False, translate=False)
+
 
 class FRAME_UL_bake_group_members(bpy.types.UIList):
 	def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
