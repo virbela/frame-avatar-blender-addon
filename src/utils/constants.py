2526eb031abfb1c78a4ff825ae6889fb61883fd0
ranjian0 <karanjaichungwa@gmail.com>
2023-01-25 15:41:01 +0300

    asset constants

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,4 +1,9 @@
 import enum
+
+from .helpers import get_asset_dir
+
+ASSET_DIR = get_asset_dir()
+
 class COLORS(enum.Enum):
 	BLACK = (0.0, 0.0, 0.0, 1.0)
 	WHITE = (1.0, 1.0, 1.0, 1.0)
@@ -18,3 +23,21 @@ TARGET_UV_MAP = 'TargetMap'
 PAINTING_UV_MAP = 'PaintingMap'
 
 GLB_VERT_COUNT = 441
+
+
+class Assets:
+	class AvatarBase:
+		url = ASSET_DIR / "avatarbase.blend"
+		name = "Avatar"
+
+	class Materials:
+		url = ASSET_DIR / "materials.blend"
+		class BakeAO:
+			name = "bake_material_ao"
+
+		class BakeDiffuse:
+			name = "bake_material_diffuse"
+
+		class BakePreview:
+			name = "bake_material_preview"
+
