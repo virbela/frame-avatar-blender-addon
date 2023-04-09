646dd03bdb95d0a3ab314ea2b48e10cbe879233a
ranjian0 <karanjaichungwa@gmail.com>
2023-04-09 20:21:03 +0300

    draw all animated shapekeys

--- src/utils/bone_animation_viewer.py
+++ src/utils/bone_animation_viewer.py
@@ -1,6 +1,6 @@
 import bpy
 import gpu
-import numpy as np
+import bmesh
 from gpu_extras.batch import batch_for_shader
 
 from .bone_animation import BoneAnimationExporter
@@ -8,10 +8,21 @@ from .helpers import get_asset_file, get_homeomorphic_tool_state, is_dev
 
 context = bpy.context
 
+ANIMATED_SHAPE_KEYS = [
+    "Body_Human",
+    "Arm_L",
+    "Arm_R",
+    "Formal_pants_L",
+    "Formal_pants_R"
+]
+
 def view_animation(animation: str, show: bool):
     ht = get_homeomorphic_tool_state(bpy.context)
 
     dns = bpy.app.driver_namespace
+    if not dns.get('be'):
+        dns['be'] = BoneAnimationExporter(bpy.context, ht)
+
     if not dns.get('sd'):
         dns["sd"] = ShaderDrawer(ht)
 
@@ -31,34 +42,51 @@ class ShaderDrawer:
     def __init__(self, ht):
         self.ht = ht
         self.handle = None
-        self.animation_data = BoneAnimationExporter(bpy.context, ht)
+        self.animation_data = bpy.app.driver_namespace['be']
+
+    def make_mesh_batches(self, shader):
+        batches = []
+        mesh = self.ht.avatar_mesh.data
+
+        for skname in ANIMATED_SHAPE_KEYS:
+            bm = bmesh.new()
+            bm.from_mesh(mesh)
+            shape = bm.verts.layers.shape[skname]
+
+            vertices = []
+            bm.verts.ensure_lookup_table()
+            for v in bm.verts:
+                vertices.append(v[shape].to_tuple(3))
+            
+            indices = []
+            bm.faces.ensure_lookup_table()
+            for f in bm.faces:
+                indices.append([v.index for v in f.verts])
+            bm.free()
+
+            batch = batch_for_shader(
+                shader, 'TRIS',
+                {"position": vertices, "color": [(0, 1, 0, 1) for _ in range(len(vertices))]},
+                indices=indices,
+            )
+
+            batch_verts = batch_for_shader(
+                shader, 'POINTS',
+                {"position": vertices, "color": [(0, 0, 0, 1) for _ in range(len(vertices))]}
+            )
+
+            batches.extend([batch, batch_verts])
+        return batches
+
+
 
     def update(self):
-        mesh = self.ht.avatar_mesh.data
-        mesh.calc_loop_triangles()
-
-        vertices = np.empty((len(mesh.vertices), 3), 'f')
-        indices = np.empty((len(mesh.loop_triangles), 3), 'i')
-
-        mesh.vertices.foreach_get(
-            "co", np.reshape(vertices, len(mesh.vertices) * 3))
-        mesh.loop_triangles.foreach_get(
-            "vertices", np.reshape(indices, len(mesh.loop_triangles) * 3))
-
 
         vertexshader = get_asset_file("bone_animation.vert.glsl", 'r')
         fragmentshader = get_asset_file("bone_animation.frag.glsl", 'r')
         shader = gpu.types.GPUShader(vertexshader, fragmentshader)
-        batch = batch_for_shader(
-            shader, 'TRIS',
-            {"position": vertices, "color": [(0, 1, 0, 1) for _ in range(len(mesh.vertices))]},
-            indices=indices,
-        )
 
-        batch_verts = batch_for_shader(
-            shader, 'POINTS',
-            {"position": vertices, "color": [(0, 0, 0, 1) for _ in range(len(mesh.vertices))]}
-        )
+        batches = self.make_mesh_batches(shader)
 
         def draw():
             gpu.state.depth_test_set('LESS_EQUAL')
@@ -67,8 +95,8 @@ class ShaderDrawer:
             shader.uniform_float("model", self.ht.avatar_mesh.matrix_world)
             shader.uniform_float("view", context.region_data.view_matrix)
             shader.uniform_float("projection", context.region_data.window_matrix)
-            batch.draw(shader)
-            batch_verts.draw(shader)
+            for batch in batches:
+                batch.draw(shader)
             gpu.state.depth_mask_set(False)
 
 
