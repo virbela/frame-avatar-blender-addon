68fa37dde6f639a12fe1dfc58f2cc97fa8a5d8ad
ranjian0 <karanjaichungwa@gmail.com>
2023-04-10 19:58:15 +0300

    argh, uniforms in blender gpushader need to have atleast one usage or a value error is thrown. RIDICULOUS!

--- src/assets/bone_animation.vert.glsl
+++ src/assets/bone_animation.vert.glsl
@@ -3,17 +3,27 @@ in vec3 position;
 in vec4 bone_weights;
 in vec4 bone_indices;
 
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform mat4 mvp;
+uniform mat4 bones[31];
 
 out vec4 fcolor;
 
 int isMatZero(mat4 m);
 
 void main() {
+    bones[0];
     fcolor = color;
-    gl_Position = projection * view * model * vec4(position, 1.0);
+
+    mat4 bone_mat = mat4(0.0);
+    for (int i = 0; i < 4; i++) {
+        bone_mat += bone_weights[int(bone_indices[i])] * bones[int(bone_indices[i])];
+    }
+
+    if (isMatZero(bone_mat) == 1) {
+        bone_mat = mat4(1.0);
+    }
+
+    gl_Position = mvp * bone_mat * vec4(position, 1.0);
 }
 
 int isMatZero(mat4 m) {
