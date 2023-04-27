b67a09ce7e2b9a4bd03385564cc04ce05bf86fe5
ranjian0 <karanjaichungwa@gmail.com>
2023-04-27 23:05:39 +0300

    update debug scripts

--- scripts/debug.py
+++ scripts/debug.py
@@ -4,15 +4,15 @@ from pathlib import Path
 THISDIR = Path(__file__).parent
 sys.path.insert(0, str(THISDIR.parent.absolute()))
 
+from src import register, unregister
 
 def main():
-    import src
-    src.register()
+    register()
+    
     bpy.ops.wm.open_mainfile(filepath=str(THISDIR.absolute() / "debug.blend"))
+    bpy.ops.faba.export()
 
-    bpy.ops.frame.export()
-
-    src.unregister()
+    unregister()
 
     files = list(THISDIR.glob("*.glb"))
     files += list(THISDIR.glob("*.npy"))
